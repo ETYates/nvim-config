a7b3aa5c6e1b743eb25a8dcea05c552e1637834d
ETYates <ethan.t.yates@gmail.com>
2026-04-12 21:06:58 -0400

    Added cursorword.

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,6 +4,9 @@ return {
     config = function()
         require('mini.ai').setup { n_lines = 500 }
         require('mini.surround').setup()
+        require('mini.cursorword').setup({
+            delay = 0,
+        })
         require("mini.pairs").setup()
         local statusline = require 'mini.statusline'
         statusline.setup()
